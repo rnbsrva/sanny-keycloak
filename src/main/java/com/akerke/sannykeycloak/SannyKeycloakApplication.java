e84178a04e2b8477f346396184973736f9d198dd
rnbsrva <orynbasarova_a@ezdrav.kz>
2025-07-23 07:21:36 +0500

    init

---
+++ src/main/java/com/akerke/sannykeycloak/SannyKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.akerke.sannykeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SannyKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SannyKeycloakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akerke/sannykeycloak/SannyKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akerke.sannykeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SannyKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
